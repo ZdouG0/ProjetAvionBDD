d096aa1140407c3365a05bdc7a841b8e62cbc24b
Milan Hergott <milan.hergott@etu.univ-tours.fr>
2024-06-11 16:23:56 +0200

    yassine

--- ClssPerson.py
+++ ClssPerson.py
@@ -6,54 +6,25 @@ class CPerson(object) :
     def __init__(self) :
         pass
     
-    def signin(self):
+    def signin(self, nom,prenom,email,password):
         print("Inscription")
-        nom = input("Entrez votre Nom d'utilisateur :")
-        print("\n")
-        phone = input("Entrez votre numero de telephone :")
-        print("\n")
-        EmailOK = False
-        while EmailOK==False :
-            email = input("Entrez votre email :")
-            sql = text("SELECT * from Users WHERE Users.UserEmail ='"+email+"'") 
-            result = session.execute(sql) 
-            if result.rowcount == 0:
-                EmailOK=True
-            else :
-                print("Un compte est deja associé a cette adresse email.\n")
+        CUsers(email,password,None,prenom+nom)
 
-        print("\n")
-        PasswordOk = False
-        while PasswordOk == False :
-            password = input("Entrez votre mots de passe :")
-            print("\n")
-            password2 = input("Entrez a nouveau votre mots de passe :")
-            if password == password2 :
-                PasswordOk =True
-                user=Users(UserName=nom,UserEmail=email,UserPassword=password,UserPhone=phone)
-                session.add(user)
-                session.commit()
-                utilisateur = CUsers(email, password, phone,nom)
-                print("Inscription reussi, entrez 1 pour vous connecter")
-                print("\n")
-                result=0;
-                while(result!=1):
-                    result = int(input())
-                utilisateur = self.login()
-            else :
-                print ("Mots de passe different veuillez reesayer\n")
-        return utilisateur
              
 
 
-    def login(self):
+    def login(self,mail,password):
         utilisateur=0
         print("Connexion\n")
         print("Pas de compte ?\n")
         print("Entrer 1 pour vous inscrire 0 sinon")
         reponse = int(input("Entrez la reponse ici :"))
         if reponse ==1 :
-            utilisateur=self.signin()
+            print("Veuillez entrer votre nom")
+            nom = input(">>")
+            print("Veuillez entrer votre prenom")
+            prenom = input(">>")
+            utilisateur=self.signin(nom,prenom,mail,password)
         else:
             ConnexionOk=False
             while ConnexionOk==False :
